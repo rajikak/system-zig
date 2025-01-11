9ab7b589bd6319195fa20a9f9978eea93a5f79bf
Nuwan Rajika Kumarasiri <nrajikak@gmail.com>
2025-01-11 09:57:20 -0600

    add dns examples

--- dns.zig
+++ dns.zig
@@ -5,9 +5,19 @@
 // https://reintech.io/blog/implementing-a-dns-server-in-go
 // https://domenicoluciani.com/2024/05/07/create-dns-resolver.html
 // https://github.com/EmilHernvall/dnsguide
+// https://github.com/google/gopacket/blob/master/layers/dns.go
 
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 
-pub fn main() void {
-    std.debug.print("dns server in zig\n", .{});
+pub fn main() !void {
+    const ip = "127.0.0.1";
+    const port = 5882;
+    const address = try std.net.Address.parseIp(ip, port);
+
+    const tpe: u32 = posix.SOCK.STREAM;
+    const protocol = posix.IPPROTO.TCP;
+    const listener = try posix.socket(address.any.family, tpe, protocol);
+    defer posix.close(listener);
 }
