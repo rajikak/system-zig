856f16daa8e243353ce2d2765387a0246a3ec7d1
Nuwan Rajika Kumarasiri <baranasoftware@gmail.com>
2025-01-12 11:38:38 -0600

    add client

--- dns.zig
+++ dns.zig
@@ -33,7 +33,36 @@ pub const DnsServer = struct {
         const listener = try posix.socket(address.any.family, tpe, protocol);
         defer posix.close(listener);
 
-        //try posix.setsockopt(listener, posix.SQL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_init, 1)));
+        try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+        try posix.bind(listener, &address.any, address.getOsSockLen());
+        try posix.listen(listener, 128);
 
+        while (true) {
+            var client_address: net.Address = undefined;
+            var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+            const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
+                print("error accepting connection: {}\n", .{err});
+                continue;
+            };
+            defer posix.close(socket);
+
+            print("{} connected\n", .{client_address});
+
+            write(socket, "hello and goodbye") catch |err| {
+                print("error writing: {}\n", .{err});
+            };
+        }
+    }
+
+    fn write(socket: posix.socket_t, msg: []const u8) !void {
+        var pos: usize = 0;
+        while (pos < msg.len) {
+            const written = try posix.write(socket, msg[pos..]);
+            if (written == 0) {
+                return error.closed;
+            }
+            pos += written;
+        }
     }
 };
