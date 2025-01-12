6efabfc10103b0ed0f0e43a2850988c53263654c
Nuwan Rajika Kumarasiri <baranasoftware@gmail.com>
2025-01-12 07:13:39 -0600

    dns server impl

--- dns.zig
+++ dns.zig
@@ -10,14 +10,30 @@
 const std = @import("std");
 const net = std.net;
 const posix = std.posix;
+const print = std.debug.print;
 
 pub fn main() !void {
-    const ip = "127.0.0.1";
-    const port = 5882;
-    const address = try std.net.Address.parseIp(ip, port);
-
-    const tpe: u32 = posix.SOCK.STREAM;
-    const protocol = posix.IPPROTO.TCP;
-    const listener = try posix.socket(address.any.family, tpe, protocol);
-    defer posix.close(listener);
+    const server = DnsServer.init("127.0.0.1", 8443);
+    try server.start();
+    print("dns server is ready\n", .{});
 }
+
+pub const DnsServer = struct {
+    ip_addr: []const u8,
+    port: u16,
+
+    pub fn init(ip_addr: []const u8, port: u16) DnsServer {
+        return DnsServer{ .ip_addr = ip_addr, .port = port };
+    }
+
+    pub fn start(self: DnsServer) !void {
+        const address = try std.net.Address.parseIp(self.ip_addr, self.port);
+        const tpe: u32 = posix.SOCK.STREAM;
+        const protocol = posix.IPPROTO.TCP;
+        const listener = try posix.socket(address.any.family, tpe, protocol);
+        defer posix.close(listener);
+
+        //try posix.setsockopt(listener, posix.SQL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_init, 1)));
+
+    }
+};
